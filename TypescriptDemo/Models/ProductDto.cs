86e5572b1f43b74c5ed99c370583f30a30c8226e
Huan Vu <dhuan.vu06@gmail.com>
2016-12-01 13:10:40 +0700

    remove unuseful files

--- TypescriptDemo/Models/ProductDto.cs
+++ TypescriptDemo/Models/ProductDto.cs
@@ -7,9 +7,15 @@ namespace TypescriptDemo.Models
 {
     public class ProductDto
     {
+        public ProductDto()
+        {
+            UpdatedDate = DateTime.Now.AddDays(-Id);
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public float UnitPrice { get; set; }
+        public DateTime UpdatedDate { get; set; }
 
         public List<ProductAttributeDto> Attributes { get; set; }
     }
